b8f5f6fd1c9c5131e41cd569229f27dea7cc66d8
Thunnder <gnorzea@gmail.com>
2022-07-13 22:09:27 -0400

    corrected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,76 +1,280 @@
-//build HTML & CSS outline 
-// 1 container , 1 static header
-//array of objects to hold questions(each q is obj)
-//start quiz function (start timer (75s) and hide welcome text/show first q)
-//getElementById for header and answer btns update txt
-//event listener that listens on clicked answers
-//check btn clicked matches answer(if matches= show correct or incorrect (-10secs))
-//update page to show nxt q
-//repeat until out of q or timer<=0
-//stop quiz function (stop timer, display score, enter initials)
-//hide q show end page
-//store initials and score in localstorage on btn click of submit
-//switch to highscore page 
-// display highscore from localstorage
+const questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        a: "strings",
+        b: "booleans",
+        c: "alerts",
+        d: "numbers",
+        correctAnswer: "alerts",
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within ____.",
+        a: "quotes",
+        b: "curly brackets",
+        c: "parentheses",
+        d: "'square brackets",
+        correctAnswer: "parentheses",
+    },
+    {
+        question: "Arrays in Javascript can be used to store ____.",
+        a: "numbers and strings",
+        b: "other arrays",
+        c: "booleans",
+        d: "all of the above",
+        correctAnswer: "all of the above",
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        a: "commas",
+        b: "curly brackets",
+        c: "quotes",
+        d: "parenthesis",
+        correctAnswer: "quotes",
+    },
+    {
+        question: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        a: "Javascript",
+        b: "terminal / bash",
+        c: "for loops",
+        d: "console log",
+        correctAnswer: "console log",
+    },
+];
 
-var countDown = document.getElementById("countdown")
-var main = document.getElementById("main")
-var startBtn = document.getElementById("start_btn")
+var welcomeHeader = document.getElementById("welcomeHeader");
+var welcomeText = document.getElementById("welcomeText");
+
+var viewHighScore = document.getElementById("viewHighScore");
+var playerScore = document.getElementById("userScore");
+var finalScore = document.getElementById("finalScore");
+
+var initialInput = document.getElementById("initialInput");
+var submitInitialBtn = document.getElementById("submitInitialBtn");
+var summary = document.getElementById("summary");
+var highScoreSection = document.getElementById("highScoreSection");
+
+var goBackBtn = document.getElementById("goBackBtn");
+var clearHighScoreBtn = document.getElementById("clearHighScoreBtn"); 
+var viewHighScore = document.getElementById("viewHighScore");
+var listOfHighScores = document.getElementById("listOfHighScores");
+
+var timeLeft = document.getElementById("timeLeft");
+var timer = document.getElementById("timer");
+var timesUp = document.getElementById("timesUp");
+
+var startButton = document.getElementById("start-btn");
+var restartButton = document.getElementById("restart-btn");
+
+var container = document.getElementById("container");
+var questionContainerEl = document.getElementById("question-container");
+var questionEl = document.getElementById("question");
+
+var answerEl = document.getElementsByClassName("ans-btn");
+var answerOne = document.getElementById("answerButton1");
+var answerTwo = document.getElementById("answerButton2");
+var answerThree = document.getElementById("answerButton3");
+var answerFour = document.getElementById("answerButton4");
+var answerCheck = document.getElementById("answerCheck");
+
+var shuffledQuestions = questions.sort(() => Math.random() - .5);
+var questionCountIndex = 0;
+
+var userScorePoints = 0;
+var totalTime = 81;
+var scoreResult;
 
 
-function countdown() {
-    var timeLeft = 5;
+startButton.addEventListener("click", startGame);
 
-    var timeInterval = setInterval(function() {
-        if (timeLeft > 0){
-            countDown.textContent = timeLeft;
-            timeLeft--;
-        }
-        else if (timeLeft === 0 ){
-             countDown.textContent = "Time is up!";
-             clearInterval(timeInterval);
-        }
-        
-    }, 1000 );
-    displayQuestions();
+submitInitialBtn.addEventListener("click", function(event){ 
+    storeHighScores(event);
+});
+viewHighScore.addEventListener("click", function(event) { 
+    showHighScores(event);
+});
+
+goBackBtn.addEventListener("click", function() {
+    startButton.classList.remove("hide");
+    welcomeHeader.classList.remove("hide");
+    welcomeText.classList.remove("hide");
+    container.classList.remove("hide");
+    container.classList.remove("container");
+    questionContainerEl.classList.add("hide");
+    highScoreSection.classList.add("hide");
+});
+
+clearHighScoreBtn.addEventListener("click", function(){
+    window.localStorage.removeItem("high scores");
+    window.location.reload();
+    listOfHighScores.innerHTML = "High Scores Cleared!";
+    clearHighScoreBtn.classList.add("hide");
+    listOfHighScores.setAttribute("style", "font-family: 'Archivo', sans-serif; font-style: italic;")
+});
+
+
+for (var i = 0; i < answerEl.length; i++ ) {
+    answerEl[i].addEventListener("click", checkAnswer);
 }
 
-function displayQuestions () {
 
-    var questions = [{
 
-        question: "What is the capital of The United Kingdom?",
-        choices: ["Manchester", "Birmingham", "London", "Bristol"],
-        answer: 2
-    },
-    {
-        question: "What is the capital of The United States?",
-        choices: ["California", "Washington, D.C", "New York", "Florida"],
-        answer: 1
-    },
-    {
-        question: "What is the capital of Canada?",
-        choices: [ "Ottawa", "Toronto", "Montreal", "Quebec City"],
-        answer: 0
-    },
-    {   question: "What is the capital of Mexico?",
-        choices: ["Tijuana", "Puebla", "Oaxaca", "Mexico City"],
-        answer: 3
+function startGame() {
+    startButton.classList.add("hide");
+    welcomeHeader.classList.add("hide");
+    welcomeText.classList.add("hide");
+    container.classList.add("container");
+    timer.classList.remove("hide");
+    playerScore.classList.remove("hide");
+    questionContainerEl.classList.remove("hide");
+
+    initialInput.textContent = "";
+    questionCountIndex = 0;
+    totalTime = 80;
+    timeLeft.textContent = totalTime;
+
+
+    var startTimer = setInterval(function() {
+        totalTime--;
+        timeLeft.textContent = totalTime;
+        if(totalTime <= 0) {
+            clearInterval(startTimer);
+            gameOver();
+            if (questionCountIndex < questions.length - 1) {
+                timesUp.classList.add("hide");
+                gameOver();
+            }
+        };
+
+    },1000);
     
-    },
-    {
-        question: "What is the capital of Australia?",
-        choices: ["Canberra", "Sydney", "Perth", "Melbourne"],
-        answer: 0
+
+    showQuestion();
+};
+
+
+function showQuestion() {
+    questionEl.textContent = shuffledQuestions[questionCountIndex].question;
+    answerOne.textContent = shuffledQuestions[questionCountIndex].a;
+    answerTwo.textContent = shuffledQuestions[questionCountIndex].b;
+    answerThree.textContent = shuffledQuestions[questionCountIndex].c;
+    answerFour.textContent = shuffledQuestions[questionCountIndex].d;
+};
+
+
+function checkAnswer(event) {
+    if (event.target.textContent === shuffledQuestions[questionCountIndex].correctAnswer) {
+        playerScore.textContent = ("Score: " + userScorePoints);
+        answerCheck.classList.remove("hide");
+        answerCheck.classList.add("text-success");
+        answerCheck.textContent = "Correct!";
+       
+     }
+     else {
+        totalTime -= 10;
+        answerCheck.classList.remove("hide");
+        answerCheck.textContent = "Wrong! The correct answer was: " + " " + questions[questionCountIndex].correctAnswer;
+        answerCheck.classList.add("text-danger");
+        };
+
+        questionCountIndex++;
+
+  
+    if (questionCountIndex < shuffledQuestions.length) {
+      
+        showQuestion();
+    } else {
+        gameOver();
     }
-    ];
-    for (var i =0; i<questions.length; i++) {
-        
-    }
+
 }
 
-startBtn.addEventListener("click", function()
-{
-    countdown();
-    displayQuestions();
-})
+
+function gameOver() {
+    container.classList.add("hide");
+    answerCheck.classList.add("hide");
+    playerScore.classList.add("hide");
+    timer.classList.add("hide");
+    welcomeHeader.classList.add("hide");
+    welcomeText.classList.add("hide");
+    summary.classList.remove("hide");
+
+    finalScore.textContent = userScorePoints
+}
+
+
+
+function storeHighScores(event) {
+    event.preventDefault();
+
+    
+    
+    if (initialInput.value === "") {
+        alert("Please enter your initials!");
+        return;
+    } 
+    
+    container.classList.add("hide");
+    answerCheck.classList.add("hide");
+    playerScore.classList.add("hide");
+    timer.classList.add("hide");
+    summary.classList.remove("hide");
+    highScoreSection.classList.remove("hide");
+
+
+
+
+    var savedHighScores = localStorage.getItem("high scores");
+    var scoresArray;
+
+    if (savedHighScores === null) {
+        scoresArray = [];
+    } else {
+        scoresArray = JSON.parse(savedHighScores)
+    }
+
+    var userScore = {
+        initials: initialInput.value,
+        score: finalScore.textContent
+    };
+
+ 
+    scoresArray.push(userScore);
+
+
+    var scoresArrayString = JSON.stringify(scoresArray);
+    window.localStorage.setItem("high scores", scoresArrayString);
+    
+   
+    showHighScores();
+}
+
+
+var i = 0;
+function showHighScores() {
+
+    container.classList.add("hide");
+    startButton.classList.add("hide");
+    answerCheck.classList.add("hide");
+    playerScore.classList.add("hide");
+    timer.classList.add("hide");
+    summary.classList.add("hide");
+    welcomeHeader.classList.add("hide");
+    welcomeText.classList.add("hide");
+    highScoreSection.classList.remove("hide");
+
+    var savedHighScores = localStorage.getItem("high scores");
+
+   
+    if (savedHighScores === null) {
+        return;
+    }
+
+
+    var storedHighScores = JSON.parse(savedHighScores);
+
+    for (; i < storedHighScores.length; i++) {
+        var eachNewHighScore = document.createElement("p");
+        eachNewHighScore.innerHTML = storedHighScores[i].initials + ": " + storedHighScores[i].score;
+        listOfHighScores.appendChild(eachNewHighScore);
+    };
+};
